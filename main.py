ceb35f64babea810ae870512e85210d5fc8ad9d2
nazarltvnk <118771039+NazarLytvyniuk@users.noreply.github.com>
2022-11-22 10:04:31 +0000

    new cod

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-# Write your code below this line 👇
-print("Welcome to the Band Name Generator.")
-city_name = input("What's name of the city you grew up in?\n")
-pet_name = input("What's your pet's name\n")
-print("Your band name could be " + city_name + " " + pet_name)
+#Tip calculator
+
+print("Welcome to the tip calculator.")
+
+total_bill = input("What was the total bill? $")
+tip_percent = input("What percentage tip would you like to give? 10, 12, or 15? ")
+number_of_people = input("How many people to split the bill? ")
+
+total_bill_as_int = int(total_bill)
+tip_percent_as_int = int(tip_percent)
+number_of_people_as_int = int(number_of_people)
+
+tip = total_bill_as_int * tip_percent_as_int / 100
+bill_person = round((tip + total_bill_as_int) / number_of_people_as_int, 2)
+
+print(f"Each person should pay: ${bill_person}")
+
+
+
